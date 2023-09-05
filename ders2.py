f68941957af83a26122fea2bce71ad5584abbd7e
erhan3861 <artpencil34@gmail.com>
2023-09-05 20:10:43 +0300

    person

--- ders2.py
+++ ders2.py
@@ -1,1 +1,9 @@
-"deneme"
+# miras almak
+class Person():
+    def __init__(self, name, surname, age):
+        self.name = name
+        self.name = surname
+        self.age = age
+    
+    def show_info(self):
+        print(self.name, self.surname, self.age)
