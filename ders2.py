037d19df24e1e9799fce214494a9b0e6fbe65eb4
erhan3861 <artpencil34@gmail.com>
2023-09-05 20:20:15 +0300

    miras

--- ders2.py
+++ ders2.py
@@ -2,8 +2,20 @@
 class Person():
     def __init__(self, name, surname, age):
         self.name = name
-        self.name = surname
+        self.surname = surname
         self.age = age
     
     def show_info(self):
         print(self.name, self.surname, self.age)
+
+class Student(Person):
+    def __init__(self, name, surname, age, number):
+        super().__init__(name,surname, age)  # super kelimesi miras aldığım sınıfı abana anlatır
+        self.number = number
+
+
+p = Person("Ali", "Yıldız", 15)
+p.show_info()
+
+s = Student("İhsan", "Yılmaz", 14, 1001)
+s.show_info()
